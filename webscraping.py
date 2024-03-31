5cca86882cffc2c952e44c0a8bb05c1408daa6f5
Mateus Moraes <mateusjr_m.a@hotmail.com>
2024-03-30 23:17:14 -0300

    Update
    Para facilitar no processo de uso do programa, foi feita a implementação de uma classe, assim toda vez que for preciso fazer a extração, basta instanciar um objeto e executar o método de extração

--- webscraping.py
+++ webscraping.py
@@ -4,54 +4,41 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 
-texto_string = requests.get('https://ge.globo.com/').text
-hora_extracao = datetime.now().strftime("%d-%m-%Y %H:%M:%S")
+class ExtracaoPortal:
+    def __init__(self):
+        self.portal = None
+        
+    def extrair(self, portal):
+        self.portal = portal
+        texto_string = requests.get('https://ge.globo.com/').text
+        hora_extracao = datetime.now().strftime("%d-%m-%Y %H:%M:%S")
 
-bsp_texto = BeautifulSoup(texto_string, 'html.parser')
-lista_noticias = bsp_texto.find_all('div', attrs={'class':'feed-post-body'})
-print("Quantidade de manchetes = ", len(lista_noticias))
-lista_noticias[0]
-
-lista_noticias[0].contents[1].text.replace('"',"")
-lista_noticias[0].find('a').get('href')
-
-descricao = lista_noticias[0].contents[2].text
-
-noticia = 'noticia'
-
-descricao = None if not descricao else descricao.text
-
-metadados = lista_noticias[0].find('div', attrs={'class':'feed-post-metadata'})
-
-time_delta = metadados.find('span', attrs={'class': 'feed-post-datetime'})
-secao = metadados.find('span', attrs={'class': 'feed-post-metadata-section'})
-
-time_delta = time_delta.text if time_delta else None
-secao = secao.text if secao else None
+        bsp_texto = BeautifulSoup(texto_string, 'html.parser')
+        lista_noticias = bsp_texto.find_all('div', attrs={'class':'feed-post-body'})
     
-print('time_delta = ', time_delta)
-print('seção = ', secao)
+        dados = []
 
-dados = []
+        for noticia in lista_noticias:
+            manchete = lista_noticias[0].contents[1].text.replace('"',"")
+            link = noticia.find('a').get('href')
 
-for noticia in lista_noticias:
-    manchete = noticia.contents[1].text.replace('"',"")
-    link = noticia.find('a').get('href')
+            descricao = noticia.contents[2].text
+            if not descricao:
+                descricao = noticia.find('div', attrs={'class':'bstn-related'})
+                descricao = descricao.text if descricao else None
+            
+            metadados = noticia.find('div', attrs={'class':'feed-post-metadata'})
+            time_delta = metadados.find('span', attrs={'class': 'feed-post-datetime'})
+            secao = metadados.find('span', attrs={'class': 'feed-post-metadata-section'})
 
-    descricao = noticia.contents[2].text
-    if not descricao:
-        descricao = noticia.find('div', attrs={'class': 'bstn-related'})
-        descricao = descricao.text if descricao else None
+            time_delta = time_delta.text if time_delta else None
+            secao = secao.text if secao else None
 
-    metadados = noticia.find('div', attrs={'class':'feed-post-metadata'})
-    time_delta = metadados.find('span', attrs={'class': 'feed-post-datetime'})
-    secao = metadados.find('span', attrs={'class': 'feed-post-metadata-section'})
+            dados.append((manchete, descricao, link, secao, hora_extracao, time_delta))
 
-    time_delta = time_delta.text if time_delta else None
-    secao = secao.text if secao else None
+        df = pd.DataFrame(dados, columns=['manchete', 'descrição', 'link', 'seção', 'hora_extração', 'time_delta'])
+        return df 
 
-    dados.append((manchete, descricao, link, secao, hora_extracao, time_delta))
-
-df = pd.DataFrame(dados, columns=['manchete', 'descrição', 'link', 'seção', 'hora_extração', 'time_delta'])
-print(df.head())    
-    
+df = ExtracaoPortal().extrair("https://globoesporte.globo.com/") 
+print(df.head())
+   
